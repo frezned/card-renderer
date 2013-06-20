01b5d7501fb8ff6d8b0ada2082f4669a98e95749
Tom McLean <tom.a.mclean@gmail.com>
2013-06-20 14:08:36 +1000

    whew, big cleanup & it's working

--- render.py
+++ render.py
@@ -23,72 +23,23 @@ import progressbar
 # TODO: some of this stuff should be in the spec file
 DPI = 300
 
-A4 = (210*mm, 297*mm)
-LETTER = (11*inch, 8.5*inch)
-CARDPAGE = ((63+6)*mm, (88+6)*mm)
 CARD = (63*mm, 88*mm)
+SIZES = dict(
+	A4 = (210*mm, 297*mm),
+	LETTER = (11*inch, 8.5*inch),
+	CARD = ((63+6)*mm, (88+6)*mm)
+)
 FRAME = (63*mm, 88*mm)
 
 CARDW = CARD[0]
 CARDH = CARD[1]
 
-IMGSIZE = inch*2
-
 DATESTR = datetime.datetime.now().strftime("%d-%b-%Y")
 
-# TODO: dumb this down in favour of specfile
-#		just note URL-types, fetch & mod dates
-class Card:
-
-	def __init__(self, data):
-		self.title = data.get('title', "")
-		self.copy = data.get('text', "")
-		if 'cmyk' in data:
-			self.imageurl = data['cmyk']
-			self.cmyk = True
-		else:
-			self.imageurl = data.get('art', "")
-			self.cmyk = False
-		self.cid = data.get('id', "X")
-		self.type = data['type']
-		self.localimage = None
-		self.chash = data.get('hash', "") or self.cid
-		self.cname = "{0}_{1}".format(self.cid, self.chash or "")
-		self.data = data
-
-	def retrieve_image(self):
-		if self.imageurl:
-			c = ""
-			if self.cmyk: c = "-cmyk"
-			img = os.path.join("download"+c, self.cname + os.path.splitext(self.imageurl)[1])
-			if not os.path.exists(img):
-				print "Fetch", self.imageurl
-				r = urllib2.urlopen(self.imageurl)
-				downf = "download/temp"
-				with open(downf, "wb") as f:
-					buf = r.read(128)
-					while buf:
-						f.write(buf)
-						buf = r.read(128)
-				r.close()
-				os.rename(downf, img)
-			self.localimage = img
-
-	def convert_image(self):
-		if self.localimage and not self.convertedimage:
-			outfn = os.path.splitext(os.path.split(self.localimage)[1])[0] + ".tif"
-			outfn = os.path.join("gen", outfn)
-			if not os.path.exists(outfn):
-				cardimgs = int(IMGSIZE/inch*DPI)
-				art = Image.open(self.localimage).resize((cardimgs, cardimgs), Image.ANTIALIAS)
-				self.localimage = outfn
-				art.save(self.localimage)
-
-	def __getitem__(self, key):
-		if key == 'filename':
-			return self.localimage
-		else:
-			return self.data.get(key, "")
+import base64, hashlib
+def hash(string):
+	hasher = hashlib.sha1(string)
+	return base64.urlsafe_b64encode(hasher.digest()[0:10]).replace("=", "")
 
 class TemplateItem:
 
@@ -106,6 +57,9 @@ class TemplateItem:
 		self.y = coordormid('y', 0.5*(CARDH-self.height))
 		if data.get('hcenter', False):
 			self.x = (CARDW-self.width) / 2
+	
+	def prepare(self, data):
+		pass
 
 class GraphicTemplateItem(TemplateItem):
 	
@@ -113,8 +67,37 @@ class GraphicTemplateItem(TemplateItem):
 		TemplateItem.__init__(self, data)
 		self.filename = data.get('filename', "")
 
+	def get_local_url(self, data):
+		url = Formatter().vformat(self.filename, [], data)
+		if url and url.startswith("http"):
+			pre, post = os.path.split(url)
+			filename = hash(pre) + "_" + post
+			return os.path.join("download", filename)
+		else:
+			return url
+
+	def prepare(self, data):
+		url = Formatter().vformat(self.filename, [], data)
+		factor = 300 * (1 / inch)
+		size = (int(self.width*factor), int(self.height*factor))
+		if url and url.startswith("http"):
+			outfn = self.get_local_url(data)
+			if not os.path.exists(outfn):
+				print "Fetch", url
+				r = urllib2.urlopen(url)
+				downf = tempfile.mktemp()
+				with open(downf, "wb") as f:
+					buf = r.read(128)
+					while buf:
+						f.write(buf)
+						buf = r.read(128)
+				r.close()
+				art = Image.open(downf).resize(size, Image.ANTIALIAS)
+				print outfn
+				art.save(outfn)
+
 	def render(self, canvas, data):
-		filename = Formatter().vformat(self.filename, [], data)
+		filename = self.get_local_url(data)
 		if os.path.exists(filename):
 			canvas.drawImage(filename, self.x, self.y, self.width, self.height)
 
@@ -151,17 +134,60 @@ class Template:
 				self.items.append(GraphicTemplateItem(e))
 			else:
 				self.items.append(TextTemplateItem(e, builder.styles))
+		self.cards = []
 
 	def render(self, canvas, data):
 		for i in self.items:
 			i.render(canvas, data)
+	
+	def prepare(self, data):
+		for i in self.items:
+			i.prepare(data)
 
-class DeckBuilder:
+class CardRenderer:
 
-	def __init__(self, data):
-		self.name = data.get('name', "")
-
+	def __init__(self):
+		self.decks = {}
 		self.styles = {}
+		self.templates = {}
+		self.keytemplates = {}
+		self.readfiles = set()
+		self.outputs = []
+		self.name = ""
+
+		self.notestyle = ParagraphStyle('note')
+
+	def prepare_canvas(self, pagesize, margin=(0,0)):
+		self.pagesize = pagesize
+		self.x = 0
+		self.y = 0
+		self.columns = int((pagesize[0]-margin[0]*2) / CARDW)
+		self.rows = int((pagesize[1]-margin[1]*2) / CARDH)
+		self.offsetx = (pagesize[0] - self.columns*CARDW) * 0.5
+		self.offsety = (pagesize[1] - CARDH) - (pagesize[1] - self.rows*CARDH) * 0.5
+		self.tempfile = tempfile.mktemp()
+		self.canvas = canvas.Canvas(self.tempfile, pagesize=(pagesize[0], pagesize[1]))
+		self.frames = {x: ("frame_{type}.tif".format(type=x)) for x in ("IT", "LO", "MO")}
+		self.background = False
+
+	def render_card(self, template, card):
+		# ensure we're on a prepared page
+		self.startpage()
+		# render the card
+		c = self.canvas
+		c.saveState()
+		c.translate(self.offsetx + self.x*CARDW, self.offsety - self.y*CARDH)
+		template.render(c, card)
+		c.restoreState()
+		# advance the card position
+		self.x += 1
+		if self.x >= self.columns:
+			self.x = 0
+			self.y += 1
+			if self.y >= self.rows:
+				self.endpage()
+
+	def parse_templates(self, data):
 		for sd in data.get('styles', []):
 			name = sd.get('name', "")
 			s = ParagraphStyle(name)
@@ -178,57 +204,10 @@ class DeckBuilder:
 				s.leading = sd['leading']
 			self.styles[name] = s
 
-		self.templates = {}
-		self.keytemplates = {}
 		for td in data.get('templates', []):
 			t = Template(td, self)
 			self.templates[t.name] = t
 			self.keytemplates[t.key] = t
-
-	def render_card(self, canvas, card):
-		canvas.saveState()
-		template = self.keytemplates[card.type]
-		template.render(canvas, card)
-		canvas.restoreState()
-
-class CardRenderer:
-
-	def __init__(self):
-		self.cards = []
-
-		with open("storywar.yaml", 'r') as f:
-			data = yaml.load(f)
-			self.db = DeckBuilder(data)
-
-	def prepare_canvas(self, pagesize, margin):
-		self.pagesize = pagesize
-		self.x = 0
-		self.y = 0
-		self.columns = int((pagesize[0]-margin[0]*2) / CARDW)
-		self.rows = int((pagesize[1]-margin[1]*2) / CARDH)
-		self.offsetx = (pagesize[0] - self.columns*CARDW) * 0.5
-		self.offsety = (pagesize[1] - CARDH) - (pagesize[1] - self.rows*CARDH) * 0.5
-		self.tempfile = tempfile.mktemp()
-		self.canvas = canvas.Canvas(self.tempfile, pagesize=(pagesize[0], pagesize[1]))
-		self.frames = {x: ("frame_{type}.tif".format(type=x)) for x in ("IT", "LO", "MO")}
-		self.background = False
-
-	def render_card(self, card):
-		# ensure we're on a prepared page
-		self.startpage()
-		# render the card
-		c = self.canvas
-		c.saveState()
-		c.translate(self.offsetx + self.x*CARDW, self.offsety - self.y*CARDH)
-		self.db.render_card(c, card)
-		c.restoreState()
-		# advance the card position
-		self.x += 1
-		if self.x >= self.columns:
-			self.x = 0
-			self.y += 1
-			if self.y >= self.rows:
-				self.endpage()
 
 	def drawGuides(self):
 		GUIDECOLOR = (0.5, 0.5, 0.5)
@@ -254,8 +233,8 @@ class CardRenderer:
 
 	def note(self):
 		if self.notefmt:
-			note = self.notefmt.format(date=DATESTR)
-			p = Paragraph(note, self.copystyle)
+			note = self.notefmt.format(date=DATESTR, name=self.name)
+			p = Paragraph(note, self.notestyle)
 			tx, ty = p.wrap(CARDW, 100)
 			p.drawOn(self.canvas, self.offsetx, self.offsety + CARDH + 0.1*cm)
 
@@ -263,8 +242,8 @@ class CardRenderer:
 		if not self.page:
 			if self.drawbackground:
 				# render background
-				c.setFillColorRGB(0, 0, 0)
-				c.rect(0, 0, self.pagesize[0], self.pagesize[1], stroke=0, fill=1)
+				self.canvas.setFillColorRGB(0, 0, 0)
+				self.canvas.rect(0, 0, self.pagesize[0], self.pagesize[1], stroke=0, fill=1)
 				self.background = True
 			self.page = True
 
@@ -279,38 +258,32 @@ class CardRenderer:
 			self.canvas.showPage()
 			self.page = False
 
-	def add_card(self, c):
-		self.cards.append(c)
-
 	def all_cards_progress(self, function):
 		i = 0
+		cards = []
+		for t in self.templates.values():
+			for c in t.cards:
+				cards.append((t, c))
 		widgets = [progressbar.Percentage(), ' ', progressbar.Bar(marker='=', left='[', right=']')]
-		bar = progressbar.ProgressBar(widgets=widgets, maxval=len(self.cards))
+		bar = progressbar.ProgressBar(widgets=widgets, maxval=len(cards))
 		bar.start()
-		for c in self.cards:
-			function(c)
+		for c in cards:
+			function(*c)
 			i += 1
 			bar.update(i)
 		bar.finish()
 
-	def prepare_cards(self, cmyk):
-		# sort cards
-		self.cards.sort(key=lambda c: c.type + (c.title or "zzz"))
+	def prepare_cards(self):
 		# ensure the destination folders exist
 		def mkdir(n):
 			if not os.path.exists(n): os.mkdir(n)
 		mkdir("download")
-		mkdir("download-cmyk")
 		mkdir("tif")
 		# prepare the cards
-		print "Retrieving card art..."
-		def retrieve(c):
-			c.retrieve_image()
-		self.all_cards_progress(retrieve)
-		print "Converting to CMYK..."
-		def convert(c):
-			c.convert_image()
-		self.all_cards_progress(convert)
+		print "Preparing card art..."
+		def prepare(t, c):
+			t.prepare(c)
+		self.all_cards_progress(prepare)
 		self.page = False
 
 	def save(self, outfile):
@@ -328,59 +301,78 @@ class CardRenderer:
 				outfile = "{0}_{1}{2}".format(fn, amt, ext)
 
 	def render(self, pagesize, outfile, note='', guides=True, drawbackground=False):
-		self.prepare_canvas(pagesize, (0, 0))
+		self.prepare_canvas(pagesize)
 		self.notefmt = note
 		self.guides = guides
 		self.drawbackground = drawbackground
 		print "Rendering to {out}...".format(out=outfile)
-		def render(c):
-			self.render_card(c)
+		def render(t, c):
+			self.render_card(t, c)
 		self.all_cards_progress(render)
 		self.endpage()
 		self.save(outfile)
+
+	def readfile(self, filename):
+		if filename not in self.readfiles:
+			self.readfiles.add(filename)
+			data = loaddata(filename)
+			self.parse_use(data)
+			self.parse_output(data)
+			self.parse_templates(data)
+			self.parse_decks(data)
+			self.name = data.get('name', self.name)
+
+	def parse_use(self, data):
+		if 'use' in data:
+			use = data['use']
+			if type(use) == str:
+				self.readfile(use)
+			else:
+				for u in use:
+					self.readfile(u)
+
+	def parse_output(self, data):
+		for o in data.get('output', []):
+			self.outputs.append(o)
+
+	def parse_decks(self, data):
+		for d in data.get('decks', []):
+			template = self.templates[d['template']]
+			for c in d.get('cards', []):
+				template.cards.append(c)
+		for c in data.get('cards', []):
+			template = self.keytemplates[c['type']]
+			template.cards.append(c)
+
+	def run(self):
+		self.prepare_cards()
+		for o in self.outputs:
+			self.render(
+					pagesize=SIZES[o.get('size', "A4")],
+					outfile = o['filename'].format(name=self.name, date=DATESTR).replace(" ", ""),
+					note = o.get("note", ""),
+					guides = o.get("guides", True),
+					drawbackground = o.get("background", False)
+				)
 
 def loaddata(datafile):
 	if datafile.startswith("http://") or datafile.startswith("https://"):
 		print "Downloading", datafile
 		r = urllib2.urlopen(datafile)
-		data = json.load(r)
+		data = yaml.load(r)
 		r.close()
 		return data
 	else:
 		with open(datafile) as f:
-			data = json.load(f)
+			data = yaml.load(f)
 			return data
 
-def main(datafile, note, cmyk, outfile, pnp, card, blanks):
+def main(datafile):
 	maker = CardRenderer()
-	data = loaddata(datafile)
-	for d in data['cards']:
-		maker.add_card(Card(d))
-	if blanks:
-		amount = 112 - len(maker.cards)
-		if amount < 0 or amount % 4:
-			raise Exception("weird number of cards")
-		else:
-			blankc = amount/4
-			blankcounts = dict(MO=blankc*2, IT=blankc, LO=blankc)
-			print blankcounts
-			for t, amt in blankcounts.items():
-				[maker.add_card(Card(dict(type=t))) for i in range(amt)]
-	maker.prepare_cards(cmyk)
-	if pnp:
-		maker.render(A4, "%s_A4_%s.pdf" % (outfile, DATESTR), note=note)
-		maker.render(LETTER, "%s_LTR_%s.pdf" % (outfile, DATESTR), note=note)
-	if card:
-		maker.render(CARDPAGE, "%s_CARD_%s.pdf" % (outfile, DATESTR), guides=False, drawbackground=True)
+	maker.readfile(datafile)
+	maker.run()
 
 if __name__ == "__main__":
 	parser = optparse.OptionParser()
-	parser.add_option("-n", "--note", dest="note", action="store", default="Story War Print and Play {date}")
-	parser.add_option("-c", "--cmyk", dest="cmyk", action="store_true", default=False, help="Convert images to CMYK.")
-	parser.add_option("-o", "--outfile", dest="outfile", action="store")
-	parser.add_option("--nopnp", dest="pnp", action="store_false", default=True)
-	parser.add_option("--nocard", dest="card", action="store_false", default=True)
-	parser.add_option("--noblanks", dest="blanks", action="store_false", default=True)
 	(options, args) = parser.parse_args()
-
-	main(args[0], note=options.note, cmyk=options.cmyk, outfile=options.outfile, blanks=options.blanks, pnp=options.pnp, card=options.card)
+	main(args[0])
